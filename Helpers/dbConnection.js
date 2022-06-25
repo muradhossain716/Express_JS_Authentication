075e1d469bb42f6a43db2ae612063559cf25879f
Rafiul29 <rafiulislam4472@gmail.com>
2022-06-25 12:26:44 +0600

    databases added

--- Helpers/dbConnection.js
+++ Helpers/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost/todos',()=>{
+mongoose.connect('mongodb+srv://konga:konga@cluster0.ak8uw.mongodb.net/?retryWrites=true&w=majority',()=>{
     console.log('db connected')
 })
 
